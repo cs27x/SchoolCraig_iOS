43f580097ef43223cdd839d9ea2271313b0d910f
Oliver Dormody <odormody@gmail.com>
2014-10-23 13:03:19 -0500

    Initial posting cell work

---
+++ SchoolCraig/SCPostingTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SCPostingTableViewCell.swift
+//  SchoolCraig
+//
+//  Created by Oliver Dormody on 10/23/14.
+//  Copyright (c) 2014 Brendan McNamara. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SCPostingTableViewCell : UITableViewCell {
+
+}
