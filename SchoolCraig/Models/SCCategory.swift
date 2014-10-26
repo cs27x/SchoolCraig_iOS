4cb1aab7433597f74e9f39be27461175df4f242c
Brendan McNamara <bahai9@live.com>
2014-10-26 13:31:55 -0500

    Defines SCPosting class

---
+++ SchoolCraig/Models/SCCategory.swift
@@ -0,0 +1,18 @@
+//
+//  SCCategory.swift
+//  SchoolCraig
+//
+//  Created by Brendan McNamra on 10/26/14.
+//  Copyright (c) 2014 Brendan McNamara. All rights reserved.
+//
+
+import Foundation
+
+enum SCCategory {
+    // These are temporary categories, may change
+    // later.
+    case Kitchen
+
+    case Dorm
+
+}
