6c1c5987021d9a0892b47a43a7f23e0faa9fb65e
grandyang <xiaochenyang@XiaochendeMacBook-Pro.local>
2014-11-02 16:18:24 -0600

    Add some API

--- SchoolCraigTests/SCRequestTests.swift
+++ SchoolCraigTests/SCRequestTests.swift
@@ -10,12 +10,12 @@ import UIKit
 import XCTest
 
 class SCRequestTests: XCTestCase {
-
+    
     func testLoginRequest() {
         var callbackIsCalled = false;
         var request = SCLoginRequest(email: "brendan.d.mcnamara@vanderbilt.edu",
-                                     password: "password")
-
+            password: "password")
+        
         request.onSuccess = {(var userArray) -> () in
             callbackIsCalled = true;
             if let _userArray = userArray {
@@ -33,14 +33,35 @@ class SCRequestTests: XCTestCase {
         
         var networkStore = SCLocalNetworkStore(waitTimeInSeconds: 0)
         networkStore.handleRequest(request)
-
+        
         XCTAssertTrue(callbackIsCalled, "Expect onSuccess to be called for login request")
     }
     
-
+    
     func testAllPostingsRequest() {
+        var callbackIsCalled = false;
+        var request = SCAllPostingsRequest()
         
+        request.onSuccess = {(var postingArray) -> () in
+            callbackIsCalled = true;
+            if let _postingArray = postingArray {
+                XCTAssertEqual(4, countElements(_postingArray), "Array should contain 4 user")
+            }
+            else {
+                XCTFail("Array optional from login request should not be nil optional")
+            }
+        }
+        
+        request.onError = {(var error) -> () in
+            callbackIsCalled = true;
+            XCTFail("login request onError was executed: \(error)")
+        }
+        
+        var networkStore = SCLocalNetworkStore(waitTimeInSeconds: 0)
+        networkStore.handleRequest(request)
+        
+        XCTAssertTrue(callbackIsCalled, "Expect onSuccess to be called for login request")
     }
-
-
+    
+    
 }
